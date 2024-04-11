0b40bf45e0beed0e7bfc027bb3070939a4124a8b
Rudic <rohalov777@gmail.com>
2024-04-11 19:05:34 +0300

    add PasswordHasher

---
+++ TradingDiary/Services/PasswordHasher.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using TradingDiary.Models.Entities;
+
+namespace TradingDiary.Services
+{
+    public class PasswordHasher : IPasswordHasher<ApplicationUser>
+    {
+        public string HashPassword(ApplicationUser user, string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public PasswordVerificationResult VerifyHashedPassword(ApplicationUser user, string hashedPassword, string providedPassword)
+        {
+            if (!BCrypt.Net.BCrypt.Verify(providedPassword, hashedPassword))
+            {
+                return PasswordVerificationResult.Failed;
+            }
+            return PasswordVerificationResult.Success;
+        }
+    }
+}
