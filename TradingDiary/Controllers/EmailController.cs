233612f8d70839cb563b639f9bf5a7b73679da65
Rudic <rohalov777@gmail.com>
2024-04-11 20:52:59 +0300

    add EmailController

---
+++ TradingDiary/Controllers/EmailController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TradingDiary.Models.Entities;
+using TradingDiary.Services;
+
+namespace TradingDiary.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IConfiguration _config;
+
+        public EmailController(UserManager<ApplicationUser> userManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _config = configuration;
+        }
+
+        [HttpPost]
+        [Authorize(Policy = "RequireUserRole")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> SendResetPasswordMessage()
+        {
+            var userId = this.GetUserIdByClaims();
+            var user = await _userManager.FindByIdAsync(userId.ToString());
+
+            string emailSender = _config["BrevoApi:SenderEmail"];
+            string nameSender = _config["BrevoApi:SenderName"];
+
+            string subject = "Reset password";
+
+            var resetToken = await _userManager.GeneratePasswordResetTokenAsync(user);
+
+            string resetUrl = $@"http://localhost:5173/resetpassword/{resetToken}";
+            string htmlContent = $"<html><body><h1>Запит на зміну пароля</h1><p>Вітаю,{user.UserName}<p>" +
+                $"<p>Ми отримали запит на зміну паролю від твого аккаунту. Якщо ви не надсилали запит на зміну пароля, проігноруйте цей електронний лист, у вашому обліковому записі не буде внесено жодних змін. </p>" +
+                $"<p>Щоб змінити пароль, перейдіть за посиланням нижче:</p><p>{resetUrl}</p></body></html>";
+
+            EmailSender.SendEmail(nameSender, emailSender, user.UserName, user.Email, subject, htmlContent);
+            return Ok("Email send");
+        }
+    }
+}
